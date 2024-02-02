0722b71eee2fb064ded8c941d12442560d8f752e
a59aggar <a59aggar@uwaterloo.ca>
2024-02-02 10:23:04 -0500

    svc_helpers

---
+++ kern/svc_helpers.h
@@ -0,0 +1,19 @@
+#ifndef __SVC_HELPERS_H__
+#define __SVC_HELPERS_H__ 1
+
+#include <stdint.h>
+#include "task_descriptor.h"
+
+int svc_create(uint32_t priority, void (*entrypoint)());
+
+void svc_yield(TaskDescriptor *curr_task);
+
+void svc_yield_first();
+
+int svc_send(int tid, const char *msg, int msglen, char *reply, int rplen, TaskDescriptor *curr_task);
+
+void svc_receive(int *tid, char* msg, int msg_len, TaskDescriptor *curr_task);
+
+int svc_reply(int tid, const char* reply, int rplen);
+
+#endif // __KERN_H__
