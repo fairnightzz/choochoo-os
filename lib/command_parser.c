0f03a699c14c9db4b7f0f631b55378b24544833b
Zhehai Zhang <zhehai.zhang@uwaterloo.ca>
2024-02-24 17:59:24 -0500

    fix some things

--- lib/command_parser.c
+++ lib/command_parser.c
@@ -2,21 +2,21 @@
 #include <string.h>
 
 #include "command_parser.h"
-#include "rpi.h"
 
-string get_str(const char* command, uint32_t* it);
-void skip_whitespace(const char* command, uint32_t* it);
-int get_i(const char* command, uint32_t* it);
+string get_str(const char *command, uint32_t *it);
+void skip_whitespace(const char *command, uint32_t *it);
+int get_i(const char *command, uint32_t *it);
 
 CommandResult
-parse_command(string* command)
+parse_command(string *command)
 {
   // read until first whitespace character
   uint32_t it = 0;
   const char *data = get_data(command);
   string cmd_name = get_str(data, &it);
 
-  if (strcmp(get_data(&cmd_name), "tr") == 0) {
+  if (strcmp(get_data(&cmd_name), "tr") == 0)
+  {
 
     skip_whitespace(data, &it);
 
@@ -26,186 +26,195 @@ parse_command(string* command)
 
     int speed = get_i(data, &it);
 
-    if (speed > 14 || speed < 0 || train > 80 || train < 1) {
-      return (CommandResult) {
-        .command_type = ERROR_COMMAND,
-      }; 
+    if (speed > 14 || speed < 0 || train > 80 || train < 1)
+    {
+      return (CommandResult){
+          .command_type = ERROR_COMMAND,
+      };
     }
 
-
-    return (CommandResult) {
-      .command_type = TRAIN_SPEED_COMMAND,
-      .command_args = {
-        .train_speed_args = {
-          .train = train,
-          .speed = speed,
-        }
-      }
-    };
+    return (CommandResult){
+        .command_type = TRAIN_SPEED_COMMAND,
+        .command_args = {
+            .train_speed_args = {
+                .train = train,
+                .speed = speed,
+            }}};
   }
-  else if (strcmp(get_data(&cmd_name), "rv") == 0) {
+  else if (strcmp(get_data(&cmd_name), "rv") == 0)
+  {
 
     skip_whitespace(data, &it);
 
     int train = get_i(data, &it);
 
-    if (train > 80 || train < 1) {
-      return (CommandResult) {
-        .command_type = ERROR_COMMAND,
-      }; 
+    if (train > 80 || train < 1)
+    {
+      return (CommandResult){
+          .command_type = ERROR_COMMAND,
+      };
     }
 
-    return (CommandResult) {
-      .command_type = REVERSE_COMMAND,
-      .command_args = {
-        .reverse_args = {
-          .train = train,
-        }
-      }
-    };
+    return (CommandResult){
+        .command_type = REVERSE_COMMAND,
+        .command_args = {
+            .reverse_args = {
+                .train = train,
+            }}};
   }
-  else if (strcmp(get_data(&cmd_name), "sw") == 0) {
+  else if (strcmp(get_data(&cmd_name), "sw") == 0)
+  {
 
     skip_whitespace(data, &it);
 
     int switch_id = get_i(data, &it);
-    if (!(1 <= switch_id && switch_id <= 18) && !(153 <= switch_id && switch_id <= 156)) {
-      return (CommandResult) {
-        .command_type = ERROR_COMMAND,
+    if (!(1 <= switch_id && switch_id <= 18) && !(153 <= switch_id && switch_id <= 156))
+    {
+      return (CommandResult){
+          .command_type = ERROR_COMMAND,
       };
     }
-    
+
     skip_whitespace(data, &it);
 
     string switch_mode = get_str(data, &it);
 
-    if (strcmp(get_data(&switch_mode), "S") == 0) {
-      return (CommandResult) {
-        .command_type = SWITCH_COMMAND,
-        .command_args = {
-          .switch_args = {
-            .switch_id = switch_id,
-            .switch_mode = SWITCH_MODE_S
-          }
-        },
+    if (strcmp(get_data(&switch_mode), "S") == 0)
+    {
+      return (CommandResult){
+          .command_type = SWITCH_COMMAND,
+          .command_args = {
+              .switch_args = {
+                  .switch_id = switch_id,
+                  .switch_mode = SWITCH_MODE_S}},
       };
     }
-    else if (strcmp(get_data(&switch_mode), "C") == 0) {
-      return (CommandResult) {
-        .command_type = SWITCH_COMMAND,
-        .command_args = {
-          .switch_args = {
-            .switch_id = switch_id,
-            .switch_mode = SWITCH_MODE_C
-          }
-        },
+    else if (strcmp(get_data(&switch_mode), "C") == 0)
+    {
+      return (CommandResult){
+          .command_type = SWITCH_COMMAND,
+          .command_args = {
+              .switch_args = {
+                  .switch_id = switch_id,
+                  .switch_mode = SWITCH_MODE_C}},
       };
     }
-    else {
-      return (CommandResult) {
-        .command_type = ERROR_COMMAND,
+    else
+    {
+      return (CommandResult){
+          .command_type = ERROR_COMMAND,
       };
     }
-
   }
-  else if (strcmp(get_data(&cmd_name), "light") == 0) {
+  else if (strcmp(get_data(&cmd_name), "light") == 0)
+  {
     skip_whitespace(data, &it);
 
     int train = get_i(data, &it);
 
-    if (train > 80 || train < 1) {
-      return (CommandResult) {
-        .command_type = ERROR_COMMAND,
-      }; 
+    if (train > 80 || train < 1)
+    {
+      return (CommandResult){
+          .command_type = ERROR_COMMAND,
+      };
     }
 
     skip_whitespace(data, &it);
 
     string light_mode = get_str(data, &it);
 
-    if (strcmp(get_data(&light_mode), "on") == 0) {
-      return (CommandResult) {
-        .command_type = LIGHTS_COMMAND,
-        .command_args = {
-          .light_args = {
-            .train = train,
-            .state = true,
-          }
-        },
-      };
-    } else if (strcmp(get_data(&light_mode), "off") == 0) {
-      return (CommandResult) {
-        .command_type = LIGHTS_COMMAND,
-        .command_args = {
-          .light_args = {
-            .train = train,
-            .state = false,
-          }
-        },
+    if (strcmp(get_data(&light_mode), "on") == 0)
+    {
+      return (CommandResult){
+          .command_type = LIGHTS_COMMAND,
+          .command_args = {
+              .light_args = {
+                  .train = train,
+                  .state = true,
+              }},
       };
     }
-    return (CommandResult) {
+    else if (strcmp(get_data(&light_mode), "off") == 0)
+    {
+      return (CommandResult){
+          .command_type = LIGHTS_COMMAND,
+          .command_args = {
+              .light_args = {
+                  .train = train,
+                  .state = false,
+              }},
+      };
+    }
+    return (CommandResult){
+        .command_type = ERROR_COMMAND,
+    };
+  }
+  else if (strcmp(get_data(&cmd_name), "q") == 0)
+  {
+    return (CommandResult){
+        .command_type = QUIT_COMMAND,
+    };
+  }
+
+  return (CommandResult){
       .command_type = ERROR_COMMAND,
-    };
-  }
-  else if (strcmp(get_data(&cmd_name), "q") == 0) {
-    return (CommandResult) {
-      .command_type = QUIT_COMMAND,
-    };
-  }
-
-  return (CommandResult) {
-    .command_type = ERROR_COMMAND,
   };
-
 }
 
 string
-get_str(const char* command, uint32_t* it)
+get_str(const char *command, uint32_t *it)
 {
   string word = new_string();
-  while (1) {
-    char c = command[*it]; 
-    if (c == 0) break;
+  while (1)
+  {
+    char c = command[*it];
+    if (c == 0)
+      break;
 
-    if (isalnum(c)) {
+    if (isalnum(c))
+    {
       push_char(&word, c);
     }
-    else if (isspace(c)) {
+    else if (isspace(c))
+    {
       break;
     }
-    
+
     ++(*it);
   }
   return word;
 }
 
-int
-get_i(const char* command, uint32_t* it)
+int get_i(const char *command, uint32_t *it)
 {
   int number = 0;
-  while (1) {
-    char c = command[*it]; 
-    if (c == 0) break;
+  while (1)
+  {
+    char c = command[*it];
+    if (c == 0)
+      break;
 
-    if (!isdigit(c)) break;
+    if (!isdigit(c))
+      break;
 
-    number = number*10 + (c-'0');
-    
+    number = number * 10 + (c - '0');
+
     ++(*it);
   }
   return number;
 }
 
-void
-skip_whitespace(const char* command, uint32_t* it)
+void skip_whitespace(const char *command, uint32_t *it)
 {
-  while (1) {
-    char c = command[*it]; 
-    if (c == 0) break;
+  while (1)
+  {
+    char c = command[*it];
+    if (c == 0)
+      break;
 
-    if (!isspace(c)) break;
-    
+    if (!isspace(c))
+      break;
+
     ++(*it);
   }
 }
--- user/ui/helper_tasks.c
+++ user/ui/helper_tasks.c
@@ -4,46 +4,63 @@
 #include "user/io-server/interface.h"
 #include "user/clock-server/interface.h"
 #include <ctype.h>
-#include "ui/render.h"
+#include "user/ui/render.h"
 
-#define ENTER_CHARACTER     0x0d
+#define ENTER_CHARACTER 0x0d
 #define BACKSPACE_CHARACTER 0x08
 
-string cres_to_string(CommandResult cres) {
-  switch (cres.command_type) {
-    case TRAIN_SPEED_COMMAND: {
-      uint32_t train = cres.command_args.train_speed_args.train;
-      uint32_t speed = cres.command_args.train_speed_args.speed;
-      return string_format("[tr]: Setting speed for train #%u to %u", train, speed);
-    } case QUIT_COMMAND: {
-      return to_string("[q]: Exiting...");
-    } case LIGHTS_COMMAND: {
-      uint32_t train = cres.command_args.light_args.train;
-      bool state = cres.command_args.light_args.state;
-      if (state) {
-        return string_format("[light]: Turning lights ON for train #%u", train);
-      } else {
-        return string_format("[light]: Turning lights OFF for train #%u", train);
-      }
-    } case REVERSE_COMMAND: {
-      uint32_t train = cres.command_args.reverse_args.train;
-      return string_format("[rv]: Reversing direction for train #%u", train);
-    } case SWITCH_COMMAND: {
-      uint32_t switch_id = cres.command_args.switch_args.switch_id;
-      SwitchMode switch_mode = cres.command_args.switch_args.switch_mode;
-      char *out_mode = "STRAIGHT";
-      if (switch_mode != SWITCH_MODE_S) {
-        out_mode = "CURVED";
-      }
-      return string_format("[sw]: Setting switch %u to %s mode", switch_id, out_mode);
-    } default: {
-      return to_string("[ERROR]: Invalid Command. Please Try Again.");
+string cres_to_string(CommandResult cres)
+{
+  switch (cres.command_type)
+  {
+  case TRAIN_SPEED_COMMAND:
+  {
+    uint32_t train = cres.command_args.train_speed_args.train;
+    uint32_t speed = cres.command_args.train_speed_args.speed;
+    return string_format("[tr]: Setting speed for train #%u to %u", train, speed);
+  }
+  case QUIT_COMMAND:
+  {
+    return to_string("[q]: Exiting...");
+  }
+  case LIGHTS_COMMAND:
+  {
+    uint32_t train = cres.command_args.light_args.train;
+    bool state = cres.command_args.light_args.state;
+    if (state)
+    {
+      return string_format("[light]: Turning lights ON for train #%u", train);
     }
+    else
+    {
+      return string_format("[light]: Turning lights OFF for train #%u", train);
+    }
+  }
+  case REVERSE_COMMAND:
+  {
+    uint32_t train = cres.command_args.reverse_args.train;
+    return string_format("[rv]: Reversing direction for train #%u", train);
+  }
+  case SWITCH_COMMAND:
+  {
+    uint32_t switch_id = cres.command_args.switch_args.switch_id;
+    SwitchMode switch_mode = cres.command_args.switch_args.switch_mode;
+    char *out_mode = "STRAIGHT";
+    if (switch_mode != SWITCH_MODE_S)
+    {
+      out_mode = "CURVED";
+    }
+    return string_format("[sw]: Setting switch %u to %s mode", switch_id, out_mode);
+  }
+  default:
+  {
+    return to_string("[ERROR]: Invalid Command. Please Try Again.");
+  }
   }
 }
 
-
-void promptTask() {
+void promptTask()
+{
   int io_server = WhoIs(ConsoleIOAddress);
   int marklin_server = WhoIs(MarklinIOAddress);
 
@@ -51,21 +68,28 @@ void promptTask() {
 
   Putc(marklin_server, 192); // reset sensors
 
-  while (1) {
+  while (1)
+  {
     int c = Getc(io_server);
-    if (c < 0) {
+    if (c < 0)
+    {
       LOG_WARN("[Getc Error in promptTask()]: got %d", c);
       continue;
     }
 
     unsigned char ch = c;
-    if (isalnum(ch) || isblank(ch)) {
+    if (isalnum(ch) || isblank(ch))
+    {
       push_char(&prompt, ch);
       render_prompt(&prompt);
-    } else if (c == BACKSPACE_CHARACTER) {
+    }
+    else if (c == BACKSPACE_CHARACTER)
+    {
       pop_char(&prompt);
       render_prompt(&prompt);
-    } else if (c == ENTER_CHARACTER) {
+    }
+    else if (c == ENTER_CHARACTER)
+    {
       CommandResult command_result = parse_command(&prompt);
       string console_string = cres_to_string(command_result);
       render_command(&console_string);
@@ -77,9 +101,11 @@ void promptTask() {
   }
 }
 
-void sensorTask() {
+void sensorTask()
+{
   int clock_server = WhoIs(ClockAddress);
-  while (1) {
+  while (1)
+  {
     read_all_sensors(Time(clock_server));
     Delay(clock_server, 20);
   }
