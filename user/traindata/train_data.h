f9bef8fd1952370fa296c01fa5a1d772fe4c292e
Zhehai Zhang <zhehai.zhang@uwaterloo.ca>
2024-03-13 12:14:06 -0400

    train 54

--- user/traindata/train_data.h
+++ user/traindata/train_data.h
@@ -22,12 +22,11 @@ static const uint32_t TRAIN_DATA_VEL[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SPEED_CO
     {129, 169, 221, 278, 335, 403, 473, 544, 618, 658}  // 77
 };
 
-
 static const uint32_t TRAIN_DATA_STOP_DIST[TRAIN_DATA_TRAIN_COUNT][TRAIN_DATA_SPEED_COUNT] = {
     {290, 348, 455, 532, 589, 675, 810, 855, 976, 1040}, // 2 number is on back, for T2 speed 14 C10->B3 and C13->E7
     // For higher speeds 11 + , change C13 to E7
     {293, 319, 421, 490, 549, 615, 803, 790, 864, 882},   // 47 track B
-    {304, 382, 450, 551, 617, 682, 765, 818, 909, 908},   // 54 track B
+    {304, 382, 450, 551, 617, 682, 835, 870, 909, 943},   // 54 track B // D1 offset 10
     {271, 109, 126, 125, 281, 237, 460, 587, 760, 1050},  // 55 track B
     {42, 93, 164, 359, 319, 490, 580, 726, 945, 1245},    // 58 tested 14-10
     {151, 235, 205, 452, 604, 427, 779, 1042, 1370, 1700} // 77 tested 14?
